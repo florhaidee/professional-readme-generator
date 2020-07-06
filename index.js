46914836e6f4ca8b21b2c44aabb50b68ed4a7966
Florhaidee <florhaideedev@gmail.com>
2020-07-05 21:01:33 -0600

    prompt questions and generate license file

--- index.js
+++ index.js
@@ -3,25 +3,132 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 /* ---- User questions --------*/
 const promptUser = () => {
-    return inquirer.prompt([{
-        type: 'input',
-        name: 'title',
-        message: 'What is the Title of your application? (Required)',
-        validate: nameInput => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the Title of your application? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the Title of your application!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'editor',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: nameInput => {
             if (nameInput) {
                 return true;
             } else {
-                console.log('Please enter the Title of your application!');
+                console.log('Please enter a descripion of the project!');
                 return false;
+                }
             }
-        }
-    }]);
+        },
+        {
+            type: 'editor',
+            name: 'installation',
+            message: 'Provide installation instruccion (Required)',
+            validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions of the project!');
+                return false;
+                }
+            }
+        },
+        {
+            type: 'editor',
+            name: 'usage',
+            message: 'Provide usage information (Required)',
+            validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter usage information!');
+                return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What license are you using for this project? (Choose one)',
+            choices: ['GNU AGPLv3','GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'ISC', 'MIT', 'Unlicense'],
+            validate: answer => {
+                if (answer.length < 1) {
+                    console.log('Please choose one license for your project!');
+                    return false;
+                } else if (answer.length >1) {
+                    console.log('Please choose just one license for your project!');
+                    return false;
+                }
+                return true;
+            },            
+            filter: function (choice) {
+                return choice
+            }            
+        },
+        {
+            type: 'editor',
+            name: 'contributing',
+            message: 'Provide constribution guidelines (Required)',
+            validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter constribution guidelines!');
+                return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmTests',
+            message: 'Would you like to add a Tests section ?',
+            default: true
+        },
+        {
+            type: 'editor',
+            name: 'tests',
+            message: 'Provide tests instructions',
+            when: ({ confirmTests }) => confirmTests
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github Username!');
+                    return false;
+                }
+            }
+        },
+
+    ])
 };
-// array of questions for user
-const questions = [
-
-];
-
 // function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, generateMarkdown(data), err => {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,92 @@
+const fs = require('fs');
+// create a tests section
+const generateTests = data => {
+  if (!data) {
+    return '';
+  }
+
+  return `
+  ## Tests
+    ${data}
+  `;
+};
+// create aditional page for license
+const generateLicense = data => {
+  console.log(data)
+  if (data === 'GNU AGPLv3') {
+    fs.copyFile('./src/AGPLv3.txt', './dist/license.txt', err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) throw err;
+      console.log('license was copied to destination');
+    });
+  }
+  if (data === 'GNU GPLv3') {
+    fs.copyFile('./src/GPLv3.txt', './dist/license.txt', err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) throw err;
+      console.log('license was copied to destination');
+    });
+  }
+  if (data === 'GNU LGPLv3') {
+    fs.copyFile('./src/LGPLv3.txt', './dist/license.txt', err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) throw err;
+      console.log('license was copied to destination');
+    });
+  }
+  if (data === 'Mozilla Public License 2.0') {
+    fs.copyFile('./src/Mozilla-license.txt', './dist/license.txt', err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) throw err;
+      console.log('license was copied to destination');
+    });
+  }
+  if (data === 'Apache License 2.0') {
+    fs.copyFile('./src/apache-2.0.txt', './dist/license.txt', err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) throw err;
+      console.log('license was copied to destination');
+    });
+  }
+  if (data === 'Apache License 2.0') {
+    fs.copyFile('./src/apache-2.0.txt', './dist/license.txt', err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) throw err;
+      console.log('license was copied to destination');
+      
+    });
+  }
+  if (data === 'ISC') {
+    fs.copyFile('./src/ISC.txt', './dist/license.txt', err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) throw err;
+      console.log('license was copied to destination');
+    });
+  }
+  if (data === 'MIT') {
+    fs.copyFile('./src/MIT-license.txt', './dist/license.txt', err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) throw err;
+      console.log('license was copied to destination');
+    });
+  }
+  if (data === 'Unlicense') {
+    fs.copyFile('./src/unlicense.txt', './dist/license.txt', err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) throw err;
+      console.log('license was copied to destination');
+    });
+  }
+};
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data)
+  const license = data.license.toString();
+  generateLicense(license);
+
+  return `# ${data.title}             ![badmath](https://img.shields.io/static/v1?label=License&message=${license}&color=blue)
   ## Description
-    $data.description
+    ${data.description}
 
   ## Table of Contents 
   * [Installation](#installation)
@@ -13,17 +97,26 @@ function generateMarkdown(data) {
   * [Questions](#questions)
   
   ## Instalation
-    $data.description
+    ${data.installation}
 
   ## Usage
+    ${data.usage}
 
   ## License
+    Copyright © ${new Date().getFullYear()} ${data.github}. 
+    Licensed under the [${license} license](./dist/license.txt). 
 
   ## Contributing
+    ${data.contributing}
 
-  ## Test
+${generateTests(data.tests)}
 
   ## Questions
+    If you have more questions about this application, you can cantact me by:
+      email: ${data.email}
+      GitHub username: ${data.github}
+
+  ## Made by ${data.github}
 `;
 }
 
